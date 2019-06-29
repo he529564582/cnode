c55995c7c4295147e6724a567b2a698615d7c856
he529564582 <he529564582@163.com>
2019-06-29 20:46:41 +0800

    add header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,44 +1,48 @@
 <template>
-  <div class="header">
-    <img src="../assets/cnodejs_light.svg" alt="">
-    <ul>
-      <li><a href="">首页</a></li>
-      <li><a href="">新手入门</a></li>
-      <li><a href="">API</a></li>
-      <li><a href="">关于</a></li>
-      <li><a href="">注册</a></li>
-      <li><a href="">登录</a></li>
-    </ul>
-  </div>
+    <div class="header">
+      <router-link  :to="{name:'root'}">
+        <img src="../assets/cnodejs_light.svg" alt="">
+      </router-link>
+      <ul>
+        <li><router-link  :to="{name:'root'}">首页</router-link></li>
+        <li><a href="#">新手入门</a></li>
+        <li><a href="#">API</a></li>
+        <li><a href="#">关于</a></li>
+        <li><a href="#">注册</a></li>
+        <li><a href="#">登录</a></li>
+      </ul>
+    </div>
 </template>
+
 <script>
-  export default {
-    name:'Header'
-  }
+    export default {
+        name: "Header"
+    }
 </script>
+
 <style scoped>
-  .header {
-    background-color: #5a5555;
-    height: 50px;
-  }
-  img{
-    max-width: 120px;
-    margin:  10px 50px;
-  }
-  ul {
-    list-style-type: none;
+.header{
+  background-color:#5a5555 ;
+  height: 50px;
+}
+img{
+  max-width: 120px;
+  margin-left: 50px;
+  margin-top: 10px;
+}
+  ul{
+    list-style: none;
     float: right;
     margin: 4px;
   }
-  li {
+  li{
     display: inline-block;
     padding: 10px 15px;
   }
-  a {
+  a{
     text-decoration: none;
     color: #ccc;
     font-size: 14px;
-    text-shadow: 0 1px 0 black;
+    text-shadow: nonr;
   }
-
 </style>
