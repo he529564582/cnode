daf3a39a7a3f2cd4b77f6f3464af4059e725cd43
he529564582 <he529564582@163.com>
2019-06-29 20:40:13 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,35 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Article from '../components/Article'
+import PostList from '../components/PostList'
+import UserInfo from '../components/UserInfo'
+import SlideBar from '../components/SlideBar'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name:'root',
+      path:'/',
+      components:{
+        main:PostList
+      }
+    },
+    {
+      name:'post_content',
+      path:'/topic/:id&author=:name',
+      components:{
+        main:Article,
+        slidebar:SlideBar
+      }
+    },
+    {
+      name:'user_info',
+      path:'/userinfo/:name',
+      components:{
+        main:UserInfo
+      }
     }
   ]
 })
