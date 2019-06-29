bbed3a24bb1d1bfa2f5fabae03ca81b1fa371d0d
he529564582 <he529564582@163.com>
2019-06-29 23:18:24 +0800

    add getstart api 路由

--- src/router/index.js
+++ src/router/index.js
@@ -4,31 +4,47 @@ import Article from '../components/Article'
 import PostList from '../components/PostList'
 import UserInfo from '../components/UserInfo'
 import SlideBar from '../components/SlideBar'
+import Getstart from '../components/Getstart.vue'
+import Api from '../components/Api.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      name:'root',
-      path:'/',
+        name:'root',
+        path:'/',
+        components:{
+          main:PostList
+        }
+      },
+      {
+        name:'post_content',
+        path:'/topic/:id&author=:name',
+        components:{
+          main:Article,
+          slidebar:SlideBar
+        }
+      },
+      {
+        name:'user_info',
+        path:'/userinfo/:name',
+        components:{
+          main:UserInfo
+        }
+      },
+      {
+      name:'getstart',
+      path:'/getstart/',
       components:{
-        main:PostList
+        main:Getstart
       }
     },
     {
-      name:'post_content',
-      path:'/topic/:id&author=:name',
+      name:'api',
+      path:'/api/',
       components:{
-        main:Article,
-        slidebar:SlideBar
-      }
-    },
-    {
-      name:'user_info',
-      path:'/userinfo/:name',
-      components:{
-        main:UserInfo
+        main:Api
       }
     }
   ]
